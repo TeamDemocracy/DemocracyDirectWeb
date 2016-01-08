9b25dae0acdda5ed59bf544dbe870c07062766b4
marcus-r <marcus.rettig@live.se>
2016-01-08 21:38:01 +0100

    Fixed a syntax error * This error came up when project was converted to tabs

--- server/sheduled-jobs.ts
+++ server/sheduled-jobs.ts
@@ -5,26 +5,28 @@ declare var SyncedCron:any;
 
 export module BackgroundJobs {
 
-	export function loadSheduledBackgroundJobs(){
+	export function loadSheduledBackgroundJobs() {
 
 		//Schedule jobs to update reports
 		console.log(Countries);
 		SyncedCron.add({
 			name: 'Synchronize Our Data With The Source Data.',
-			schedule: function(parser:any) {
+			schedule: function(parser: any) {
 				// parser is a later.parse object
 
 				return parser.text('every 15 mins');//'every hour starting on the ' + index + 'th min');
-	},
-	job: function() {
-		//Get all the country functions
-		//console.log(Countries);
-		for (var country of Countries){
-			console.log(country)
-			country.synchronizeOurDataWithSource();
-		}
-		return "yay"
-	}
-});
+			},
+			job: function() {
+				//Get all the country functions
+				//console.log(Countries);
+				for (var country of Countries) {
+					console.log(country)
+					country.synchronizeOurDataWithSource();
+				}
+				return "yay"
+			}
+		});
 
-SyncedCron.start();
+		SyncedCron.start();
+	}
+}
